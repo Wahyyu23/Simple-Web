6c40c3cff16f627fa3945aeb36e95edb72a431fa
Wahyyu23 <jorunatenkura@gmail.com>
2023-12-11 17:21:37 +0700

    First Commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Toggle kelas active//
+// const navbarNav = document.querySelector('.navbar-nav');
+//Ketika hamburger menu di klik//
+// document.querySelector('#hamburger-menu').onclick = () => {
+    // navbarNav.classList.toggle('active');
+  // };
